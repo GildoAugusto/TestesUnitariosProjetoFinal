ca4e473989a048c340d41d75d6015c602fcd53c5
Gildo Augusto <gildo.augu@gmail.com>
2024-12-03 10:14:20 -0300

    Ajustes nas boas práticas

--- test/models/test_ice_cream_stand.py
+++ test/models/test_ice_cream_stand.py
@@ -8,12 +8,12 @@ class TestIceCreamStand:
         test_flavors = ["Morango", "Chocolate", "Creme"]
         icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
         icecream.open_restaurant()
+        resultado_esperado = "\nNo momento temos os seguintes sabores de sorvete disponíveis:"
 
         # Chamada
         resultado = icecream.flavors_available()
 
         # Validação
-        resultado_esperado = "\nNo momento temos os seguintes sabores de sorvete disponíveis:"
         for flavor in test_flavors:
             resultado_esperado += f"\t-{flavor}"
 
@@ -23,24 +23,24 @@ class TestIceCreamStand:
         # Setup
         test_flavors = []
         icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+        resultado_esperado = "Estamos sem estoque atualmente!"
 
         # Chamada
         resultado = icecream.flavors_available()
 
         # Validação
-        resultado_esperado = "Estamos sem estoque atualmente!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
 
     def test_find_flavor_success(self):
         # Setup
         test_flavors = ["Creme", "Morango", "Chocolate", "Baunilha", "Chocolate Amargo"]
         icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+        resultado_esperado = f"Temos no momento Morango!"
 
         # Chamada
         resultado = icecream.find_flavor("Morango")
 
         # Validação
-        resultado_esperado = f"Temos no momento Morango!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
 
     @pytest.mark.parametrize("test_data", [1234123,[], {}, 2.3])
@@ -48,72 +48,72 @@ class TestIceCreamStand:
         # Setup
         test_flavors = ["Creme", "Morango", "Chocolate", "Baunilha", "Chocolate Amargo"]
         icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+        resultado_esperado = f"Parametro 'flavor' invalido!"
 
         # Chamada
         resultado = icecream.find_flavor(test_data)
 
         # Validação
-        resultado_esperado = f"Parametro 'flavor' invalido!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
 
     def test_find_flavor_that_not_exist(self):
         # Setup
         test_flavors = ["Creme", "Morango", "Chocolate", "Baunilha", "Chocolate Amargo"]
         icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+        resultado_esperado = f"Não temos no momento Maracujá!"
 
         # Chamada
         resultado = icecream.find_flavor("Maracujá")
 
         # Validação
-        resultado_esperado = f"Não temos no momento Maracujá!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
 
     def test_find_flavor_there_is_no_flavors_registered(self):
         # Setup
         test_flavors = []
         icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+        resultado_esperado = f"Estamos sem estoque atualmente!"
 
         # Chamada
         resultado = icecream.find_flavor("Maracujá")
 
         # Validação
-        resultado_esperado = f"Estamos sem estoque atualmente!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
 
     def test_add_flavor_success(self):
         # Setup
         test_flavors = ["Creme", "Chocolate", "Morango", "Pistache"]
         icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+        resultado_esperado = f"Maracujá adicionado ao estoque!"
 
         # Chamada
         resultado = icecream.add_flavor("Maracujá")
 
         # Validação
-        resultado_esperado = f"Maracujá adicionado ao estoque!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
 
     def test_add_flavor_out_of_stock(self):
         # Setup
         test_flavors = []
         icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+        resultado_esperado = f"Maracujá adicionado ao estoque!"
 
         # Chamada
         resultado = icecream.add_flavor("Maracujá")
 
         # Validação
-        resultado_esperado = f"Maracujá adicionado ao estoque!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
 
     def test_add_flavor_that_already_exist(self):
         # Setup
         test_flavors = ["Maracujá"]
         icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+        resultado_esperado = f"Sabor já disponivel!"
 
         # Chamada
         resultado = icecream.add_flavor("Maracujá")
 
         # Validação
-        resultado_esperado = f"Sabor já disponivel!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
 
     @pytest.mark.parametrize("test_data", [1234123,[], {}, 2.3])
@@ -121,10 +121,10 @@ class TestIceCreamStand:
         # Setup
         test_flavors = ["Maracujá"]
         icecream = IceCreamStand("Seu Romário", "Culinária Francesa", test_flavors)
+        resultado_esperado = f"Parametro 'flavor' invalido!"
 
         # Chamada
         resultado = icecream.add_flavor(test_data)
 
         # Validação
-        resultado_esperado = f"Parametro 'flavor' invalido!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, Recebido: {resultado}"
--- test/models/test_restaurant.py
+++ test/models/test_restaurant.py
@@ -6,25 +6,24 @@ class TestRestaurant:
     def test_describe_restaurant_success(self):
         # Setup
         restaurant =  Restaurant("Seu Alfredo", "Comida Italiana")
+        resultado_esperado = (f"Esse restaurante chama {restaurant.restaurant_name} e serve {restaurant.cuisine_type}."
+                              f"Esse restaurante está servindo {restaurant.number_served} consumidores desde que está aberto.")
 
         # Chamada
         resultado = restaurant.describe_restaurant()
 
         # Validação
-        resultado_esperado = (f"Esse restaurante chama {restaurant.restaurant_name} e serve {restaurant.cuisine_type}."
-                              f"Esse restaurante está servindo {restaurant.number_served} consumidores desde que está aberto.")
         assert resultado == resultado_esperado, f"Esperado a mensagem: {resultado_esperado}, recebido: {resultado}"
 
     def test_open_restaurant(self):
         # Setup
         restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+        resultado_esperado = f"{restaurant.restaurant_name} agora está aberto!"
 
         # Chamada
         resultado = restaurant.open_restaurant()
 
         # Validação
-        resultado_esperado = f"{restaurant.restaurant_name} agora está aberto!"
-
         assert resultado == resultado_esperado, f"Esperado a mensagem: {resultado_esperado}, recebido: {resultado}"
         assert restaurant.open is True, f"Esperado: {True}, recebido: {restaurant.open}"
 
@@ -32,12 +31,12 @@ class TestRestaurant:
         # Setup
         restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
         restaurant.open_restaurant()
+        resultado_esperado = f"{restaurant.restaurant_name} já está aberto!"
 
         # Chamada
         resultado = restaurant.open_restaurant()
 
         # Validação
-        resultado_esperado =  f"{restaurant.restaurant_name} já está aberto!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
         assert restaurant.open is True, f"Esperado: {True}, recebido: {restaurant.open}"
 
@@ -45,24 +44,24 @@ class TestRestaurant:
         # Setup
         restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
         restaurant.open_restaurant()
+        resultado_esperado = f"{restaurant.restaurant_name} agora está fechado!"
 
         # Chamada
         resultado = restaurant.close_restaurant()
 
         # Validação
-        resultado_esperado =  f"{restaurant.restaurant_name} agora está fechado!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
         assert restaurant.open is False, f"Esperado: {False}, recebido: {restaurant.open}"
 
     def test_close_restaurant_for_an_already_closed_restaurant(self):
         # Setup
         restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+        resultado_esperado = f"{restaurant.restaurant_name} já está fechado!"
 
         # Chamada
         resultado = restaurant.close_restaurant()
 
         # Validação
-        resultado_esperado = f"{restaurant.restaurant_name} já está fechado!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
         assert restaurant.open is False, f"Esperado: {False}, recebido: {restaurant.open}"
 
@@ -70,12 +69,12 @@ class TestRestaurant:
         # Setup
         restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
         restaurant.open_restaurant()
+        resultado_esperado = 10
 
         # Chamada
         resultado = restaurant.set_number_served(10)
 
         # Validação
-        resultado_esperado = 10
         assert restaurant.number_served == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {restaurant.number_served}"
 
     @pytest.mark.parametrize("test_data", ["HAHAHA", [], {}, 2.3, ""])
@@ -83,23 +82,23 @@ class TestRestaurant:
         # Setup
         restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
         restaurant.open_restaurant()
+        resultado_esperado = "Parametro invalido"
 
         # Chamada
         resultado = restaurant.set_number_served(test_data)
 
         # Validação
-        resultado_esperado = "Parametro invalido"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
 
     def test_set_number_served_for_closed_restaurant(self):
         # Setup
         restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
+        resultado_esperado = f"{restaurant.restaurant_name} está fechado!"
 
         # Chamada
         resultado = restaurant.set_number_served(10)
 
         # Validação
-        resultado_esperado = f"{restaurant.restaurant_name} está fechado!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
 
     def test_increment_number_served_success(self):
@@ -108,11 +107,12 @@ class TestRestaurant:
         restaurant.open_restaurant()
         restaurant.set_number_served(10)
         restaurant.increment_number_served(10)
+        resultado_esperado = 20
+
         # Chamada
         resultado = restaurant.number_served
 
         # Validação
-        resultado_esperado = 20
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
 
     def test_increment_number_served_closed_restaurant(self):
@@ -121,12 +121,12 @@ class TestRestaurant:
         restaurant.open_restaurant()
         restaurant.set_number_served(10)
         restaurant.close_restaurant()
+        resultado_esperado = f"{restaurant.restaurant_name} está fechado!"
 
         # Chamada
         resultado = restaurant.increment_number_served(10)
 
         # Validação
-        resultado_esperado = f"{restaurant.restaurant_name} está fechado!"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
 
     @pytest.mark.parametrize("test_data", ["HAHAHA", [], {}, 2.3, ""])
@@ -135,10 +135,10 @@ class TestRestaurant:
         restaurant = Restaurant("Seu Alfredo", "Comida Italiana")
         restaurant.open_restaurant()
         restaurant.set_number_served(10)
+        resultado_esperado = "Parametro invalido"
 
         # Chamada
         resultado = restaurant.increment_number_served(test_data)
 
         # Validação
-        resultado_esperado = "Parametro invalido"
         assert resultado == resultado_esperado, f"Esperado: {resultado_esperado}, recebido: {resultado}"
